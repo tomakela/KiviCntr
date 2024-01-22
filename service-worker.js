b35386dfd3ed6f51c02c559796a48370b121c326
tomakela <teemu.o.makela@gmail.com>
2024-01-22 17:00:52 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        '/index.html',
+        '/KiviCntr/index.html',
         // other assets you want to cache
       ]);
     })
